cb1582d3c3ddded7318667d5c9289168d11ff980
Charles Painter <cpainter85@gmail.com>
2015-03-04 13:54:22 -0700

    add has_and_belongs_to_many association between doctor and patient

---
+++ db/migrate/20150304204627_create_doctors_patients.rb
@@ -0,0 +1,8 @@
+class CreateDoctorsPatients < ActiveRecord::Migration
+  def change
+    create_table :doctors_patients do |t|
+      t.belongs_to :doctor
+      t.belongs_to :patient
+    end
+  end
+end
